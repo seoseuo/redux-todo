3a9274dd998691721f42046720095f36580739d3
DESKTOP-10542SG\비컴솔루션 <skseo@becomeuniverse.com>
2025-08-05 13:37:34 +0900

    Chore : 리듀서 슬라이싱 및 콤바인리듀서 등록

--- src/reducer.js
+++ src/reducer.js
@@ -1,44 +1,11 @@
-import { fa } from 'faker/lib/locales'
+import { combineReducers } from 'redux'
 
-const initialState = {
-  todos: [
-    { id: 0, text: 'Learn React', completed: true },
-    { id: 1, text: 'Learn Redux', completed: false, color: 'purple' },
-    { id: 2, text: 'Build something fun!', completed: false, color: 'blue' },
-  ],
-  filters: {
-    status: 'All',
-    colors: [],
-  },
-}
+import todosReducer from './features/todos/todosSlice'
+import filtersReducer from './features/filters/filtersSlice'
 
-// Use the initialState as a default value
-export default function appReducer(state = initialState, action) {
-  // The reducer normally looks at the action type field to decide what happens
-  switch (action.type) {
-    // Do something here based on the different types of actions
-    case 'todos/todoAdded': {
-      return {
-        // 원본 값을 복사하고 복사본을 변형함
-        ...state,
-        todos: [
-          ...state.todos,
-          {
-            id: nextTodoId(state.todos),
-            text: action.payload,
-            completed: false,
-          },
-        ],
-      }
-    }
-    default:
-      // If this reducer doesn't recognize the action type, or doesn't
-      // care about this specific action, return the existing state unchanged
-      return state
-  }
-}
+const rootReducer = combineReducers({
+  todos: todosReducer,
+  filters: filtersReducer,
+})
 
-function nextTodoId(todos) {
-  const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1)
-  return maxId + 1
-}
+export default rootReducer
